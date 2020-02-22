dc94f3b477719292e84c3a97567118b1f018306f
Fabrício da Silva Almeida <fabricioalmeida.dev@gmail.com>
2020-02-22 09:17:14 -0400

    Ajustes na home do projeto

--- src/StoreOfBuild.Web/Controllers/HomeController.cs
+++ src/StoreOfBuild.Web/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace StoreOfBuild.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
